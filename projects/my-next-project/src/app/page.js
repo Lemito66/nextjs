ba5e47305900c34179cff52eae69d45319c62f9c
Lemito66 <lemito66@gmail.com>
2023-12-28 16:34:22 -0500

    usando use client

--- projects/my-next-project/src/app/page.js
+++ projects/my-next-project/src/app/page.js
@@ -1,9 +1,18 @@
-import Image from 'next/image'
+// https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns
+"use client"; // Así se indica que se va a usar el cliente de Next.js para renderizar la página del lado del cliente
+import { Users } from "@/components/Users";
+import Image from "next/image";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>My Next Project</h1>
+      <button onClick={() => setCount(count + 1)}>Click me {count}</button>
+      <Users />
     </div>
-  )
+  );
 }
---
+++ projects/my-next-project/src/components/Users.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+export function Users() {
+  useEffect(() => {
+    alert("Users");
+  }, []);
+  return (
+    <div>
+      <h2>Users</h2>
+    </div>
+  );
+}
