397bb99d0f86788b8ff877a9ce3e3e14ca119e75
Lemito66 <lemito66@gmail.com>
2023-12-29 15:57:02 -0500

    redireccionamiento a otras páginas

--- projects/my-next-project/src/app/about/page.jsx
+++ projects/my-next-project/src/app/about/page.jsx
@@ -1,9 +1,26 @@
-import React from "react";
-export const metadata = {
+"use client";
+import { useRouter } from "next/navigation";
+
+/* export const metadata = {
   title: "About Page",
   description: "Primera aplicación con Next.js",
   keywords: "nextjs, react, javascript, ecommerce, tienda",
-};
+}; */
 export default function AboutPage() {
-  return <h1>about</h1>;
+  const router = useRouter(); // Hook de Next.js para obtener el objeto router y poder navegar entre páginas
+  return (
+    <section>
+      <h1>About Page</h1>
+      <p>fdgffdgdfgdfgsf</p>
+
+      <button
+        className="bg-sky-500 rounded-md"
+        onClick={() => {
+          router.push("/"); // Navegar a la página de inicio
+        }}
+      >
+        Clic
+      </button>
+    </section>
+  );
 }
