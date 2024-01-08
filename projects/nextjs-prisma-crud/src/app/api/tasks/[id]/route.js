dde59b6ec20940157dfe1c074889d4003655465a
Lemito66 <lemito66@gmail.com>
2024-01-08 13:17:15 -0500

    creación del backend

--- projects/nextjs-prisma-crud/src/app/api/tasks/[id]/route.js
+++ projects/nextjs-prisma-crud/src/app/api/tasks/[id]/route.js
@@ -1,23 +1,46 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
-/* export async function GET(request, { params }) {
-  const tasks = await prisma.task.findMany();
+export async function GET({ params }) {
+  const task = await prisma.task.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
   return NextResponse.json({
-    message: "obteniendo tarea por id: ",
-    data: tasks,
+    message: "obteniendo tarea por id: " + params.id,
+    data: task,
   });
 }
 
-export async function POST(request, { params }) {
+export async function PUT(request, { params }) {
   const data = await request.json();
-  console.log(data);
+  const taskUpdated = await prisma.task.update({
+    where: {
+      id: Number(params.id),
+    },
+    data: data,
+  });
   return NextResponse.json({
-    message: "Creación de tarea exitosa",
+    message: "Actualizando tarea por id: " + params.id,
+    data: taskUpdated,
   });
 }
 
-export function PUT(request, { params }) {
-    return NextResponse.json({
-      message: "Actualizando tarea por id: " + params.id,
+export async function DELETE({ params }) {
+  try {
+    const taskRemoved = await prisma.task.delete({
+      where: {
+        id: Number(params.id),
+      },
     });
-  } */
+    return NextResponse.json({
+      message: "Eliminando tarea por id: " + params.id,
+      data: taskRemoved,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      message: "Error al eliminar tarea por id: " + params.id,
+      data: error,
+    });
+  }
+}
--- projects/nextjs-prisma-crud/src/app/api/tasks/route.js
+++ projects/nextjs-prisma-crud/src/app/api/tasks/route.js
@@ -1,24 +1,24 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
-export async function GET(request, { params }) {
+export async function GET() {
   const tasks = await prisma.task.findMany();
   return NextResponse.json({
     message: "Tareas obtenidas satisfactoriamente",
     data: tasks,
   });
 }
-export async function POST(request, { params }) {
+export async function POST(request) {
   const { title, description } = await request.json();
-  const data = await prisma.task.create({
+  const newTask = await prisma.task.create({
     data: {
       title,
       description,
     },
   });
-
-  console.log(data);
+  
   return NextResponse.json({
-    message: "Creación de tarea exitosa",
+    message: "Tarea creada satisfactoriamente",
+    data: newTask,
   });
 }
