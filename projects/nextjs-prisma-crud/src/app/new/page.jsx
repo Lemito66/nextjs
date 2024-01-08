0b7efa0e8272c996b88cc246b9e8e93ab6bab605
Lemito66 <lemito66@gmail.com>
2024-01-08 16:31:12 -0500

    formulario de ingreso

--- projects/nextjs-prisma-crud/src/app/new/page.jsx
+++ projects/nextjs-prisma-crud/src/app/new/page.jsx
@@ -1,9 +1,17 @@
-
-
 function NewPage() {
   return (
-    <div>Formulario de Tareas</div>
-  )
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10">
+        <input type="text" placeholder="Title" 
+          className="border border-gray-400 p-2 mb-4 w-full"
+        />
+        <textarea placeholder="Content" row="3" className="border border-gray-400 p-2 mb-4 w-full"></textarea>
+        <button type="submit" 
+        className="bg-green-500 text-white p-2 w-full"
+        >Crear</button>
+      </form>
+    </div>
+  );
 }
 
-export default NewPage
+export default NewPage;
