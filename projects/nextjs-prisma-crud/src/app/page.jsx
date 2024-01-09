cab586478139d8dc4fb0bf52e2e536b0251572c9
Lemito66 <lemito66@gmail.com>
2024-01-09 13:29:08 -0500

    creación de componente TaskCard

--- projects/nextjs-prisma-crud/src/app/page.jsx
+++ projects/nextjs-prisma-crud/src/app/page.jsx
@@ -7,6 +7,8 @@ async function loadTasks() {
   return tasks;
 } */
 
+import { TaskCard } from "@/component/TaskCard";
+
 // realizando una petición a la API
 async function loadTasks() {
   const res = await fetch("http://localhost:3000/api/tasks");
@@ -23,11 +25,13 @@ async function HomePage() {
     <section className="container mx-auto mt-10">
     <div className="grid grid-cols-3 gap-3">
       {tasks.data.map(({ id, title, description, createdAt }) => (
-        <div key={id} className="bg-slate-800 p-3 hover:bg-slate-800 hover:cursor-pointer">
-          <h3 className="font-bold text-2xl mb-2">{title}</h3>
-          <p>{description}</p>
-          <p className="text-sm text-gray-400">{new Date(createdAt).toLocaleDateString()}</p>
-        </div>
+        <TaskCard
+          key={id}
+          id={id}
+          title={title}
+          description={description}
+          createdAt={createdAt}
+        />
       ))}
     </div>
   </section>
