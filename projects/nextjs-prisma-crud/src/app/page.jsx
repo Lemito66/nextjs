ad6c928086cd432b52dc5c420fee0afb4bc0963c
Lemito66 <lemito66@gmail.com>
2024-01-09 13:17:48 -0500

    visualización de todas las tareas

--- projects/nextjs-prisma-crud/src/app/page.jsx
+++ projects/nextjs-prisma-crud/src/app/page.jsx
@@ -1,9 +1,37 @@
+// usando prisma
+/* import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function loadTasks() {
+  const tasks = await prisma.task.findMany();
+  console.log(tasks);
+  return tasks;
+} */
 
-
-function HomePage() {
-  return (
-    <div>Tareas</div>
-  )
+// realizando una petición a la API
+async function loadTasks() {
+  const res = await fetch("http://localhost:3000/api/tasks");
+  const data = await res.json();
+  console.log(data);
+  return data;
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTasks();
+  console.log(tasks);
+
+  return (
+    <section className="container mx-auto mt-10">
+    <div className="grid grid-cols-3 gap-3">
+      {tasks.data.map(({ id, title, description, createdAt }) => (
+        <div key={id} className="bg-slate-800 p-3 hover:bg-slate-800 hover:cursor-pointer">
+          <h3 className="font-bold text-2xl mb-2">{title}</h3>
+          <p>{description}</p>
+          <p className="text-sm text-gray-400">{new Date(createdAt).toLocaleDateString()}</p>
+        </div>
+      ))}
+    </div>
+  </section>
+  );
+}
+
+export default HomePage;
