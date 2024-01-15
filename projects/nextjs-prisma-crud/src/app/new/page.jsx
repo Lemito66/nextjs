45512a109cfc6e7ea1e84fd5786681dea0b6f103
Lemito66 <lemito66@gmail.com>
2024-01-15 10:01:15 -0500

    actualizar e insertar una tarea

--- projects/nextjs-prisma-crud/src/app/new/page.jsx
+++ projects/nextjs-prisma-crud/src/app/new/page.jsx
@@ -1,39 +1,53 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 function NewPage({ params }) {
-  //console.log(params);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const router = useRouter();
 
   useEffect(() => {
-    /* if (!params) {
-      router.push("/");
-    } */
-    /* fetch(`http://localhost:3000/api/tasks/${params.id}`)
-      .then((res) => res.json())
-      .then((data) => console.log(data)); */
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.data.title);
+          setDescription(data.data.description);
+        });
 
-    console.log(params.id);
+      console.log(params.id);
+    }
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
 
     console.log(title, description);
 
-    const res = await fetch("/api/tasks", {
-      method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    } else {
+      const res = await fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      console.log(data);
+    }
+    
     router.push("/");
+    router.refresh();
   };
   return (
     <div className="h-screen flex justify-center items-center">
@@ -49,6 +63,8 @@ function NewPage({ params }) {
           id="title"
           placeholder="Título"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
         <label htmlFor="description" className="font-bold text-sm">
           Descripción
@@ -58,6 +74,8 @@ function NewPage({ params }) {
           row="3"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
           id="description"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         ></textarea>
         <button type="submit" className="bg-green-500 text-white p-2 w-full">
           Crear
